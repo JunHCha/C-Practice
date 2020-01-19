42a50a791e0f70321f1c0897741d8bbff8637a24
JunHCha <quffl9612@naver.com>
2020-01-19 15:49:47 +0900

    Initial Commit

---
+++ C, C++ Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C, C++ Practice
+//
+//  Created by 차준형 on 2020/01/19.
+//  Copyright © 2020 차준형. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
